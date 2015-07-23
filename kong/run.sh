3e2aa526a9854494bd924de01565cfb2e1aa8563
neilellis <hello@neilellis.me>
2015-07-23 13:06:30 +0100

    Initial Commit

--- kong/run.sh
+++ kong/run.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 envsubst '$CASSANDRA_PORT_9042_TCP_ADDR:$CASSANDRA_PORT_9042_TCP_PORT' < /kong.yml > /etc/kong/kong.yml
+while ! ping $CASSANDRA_PORT_9042_TCP_ADDR
+do
+    sleep 5
+done
+
+count=1
 while ! kong start
 do
     sleep 10
+    count=$(( count + 1 ))
+    (( count < 10 ))
 done
