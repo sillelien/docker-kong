2232697b67bfe3d855b2d2c93e8984c4ff515cde
adam.m.clark <adam.m.clark@accenture.com>
2015-08-02 23:33:52 -0600

    adding host entries for linked containers

--- kong/run.sh
+++ kong/run.sh
@@ -7,6 +7,22 @@ do
     sleep 5
 done
 
+env_vars=$(env | grep ".*_NAME=" | cut -d= -f1 | tr '\n' ' ')
+echo "#Auto Generated - DO NOT CHANGE" >> /etc/hosts
+for env_var in $env_vars
+do
+  link=${env_var%_NAME}
+  domain=$(cat /etc/resolv.conf | grep search | cut -d' ' -f2)
+  nameserver=$(cat /etc/resolv.conf | grep nameserver | head -1 | cut -d' ' -f2)
+  ip=$(nslookup "${link}.${domain}" ${nameserver}  | grep Address | tail -1 | cut -d: -f2  | cut -d' ' -f2)
+  if [ -n "$ip" ]
+  then
+    echo "${ip} ${link}" >> /etc/hosts
+  else
+    logger "ip ${link}.${domain} skipped, it didn't resolve."
+  fi
+done
+
 count=1
 while ! kong start
 do
