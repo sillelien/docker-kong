5fd8d63cc4298b396b233a5d41118c9588f8740b
adam.m.clark <adam.m.clark@accenture.com>
2015-08-02 20:46:45 -0600

    Updating the kong version to 0.4.1

--- kong/build.sh
+++ kong/build.sh
@@ -4,6 +4,6 @@ set -ex
 cd /tmp
 apt-get update
 apt-get install -y netcat lua5.1 openssl libpcre3 dnsmasq curl sudo gettext
-curl -L https://github.com/Mashape/kong/releases/download/0.4.0/kong-0.4.0.wheezy_all.deb > kong-0.4.0.wheezy_all.deb
-dpkg -i kong-0.4.0.*.deb
+curl -L https://github.com/Mashape/kong/releases/download/0.4.1/kong-0.4.1.wheezy_all.deb > kong-0.4.1.wheezy_all.deb
+dpkg -i kong-0.4.1.*.deb
 rm /tmp/*
